a3632480b9e7caabb41f25a22237d4a0698ce61f
Spencer Sang <stsang333@gmail.com>
2023-04-17 16:41:01 -0700

    first commit with login functionality

---
+++ client/src/pages/Login.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function App() {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function loginUser (event) {
+    event.preventDefault()
+
+    const response = await fetch('http://localhost:1337/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    })
+
+    const data = await response.json()
+
+    if (data.user) {
+      localStorage.setItem('token', data.user)
+      alert('Login successful')
+      //window.location.href = '/dashboard'
+      navigate('/dashboard')
+    } else {
+      alert('Please check your username and password')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginUser}>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="email"
+        />
+        <br/>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+        />
+        <br/>
+        <input type="submit" value="Login"/>
+      </form>
+    </div>
+  );
+}
+
+export default App;
